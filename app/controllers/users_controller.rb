ec2f686340f959017b21f99b56b3d62721af25cc
Melissa Martinez <melmar12@live.com>
2016-10-29 19:01:35 -0500

    added secure login with bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :classification, :email, :organization, :password_digest, :admin)
+      params.require(:user).permit(:first_name, :last_name, :classification, :email, :organization, :admin, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :events
   resources :taggings
   resources :tags
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
