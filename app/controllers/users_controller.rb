95733520ca5778c5ffe0a3ed0c69cbb62a87034d
Melissa Martinez <melmar12@live.com>
2016-11-01 12:42:29 -0500

    fixed issue #1, login upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,15 +29,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+
+
+    #respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        #format.html { redirect_to @user, notice: 'User was successfully created.' }
+        #format.json { render :show, status: :created, location: @user }
+
+        session[:user_id] = @user.id
+        flash[:success] = "Signup successfully. Welcome #{@user.first_name}!"
+        redirect_to root_path
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
+        # render new
       end
-    end
+    #end
   end
 
   # PATCH/PUT /users/1
