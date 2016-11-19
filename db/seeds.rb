c309cff46f6a3106f2f17898a0cfbf6b61443c95
Melissa Martinez <melmar12@live.com>
2016-11-18 20:47:01 -0600

    more dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,5 +32,46 @@ Event.create(
     user_id: 2
 );
 
+Event.create(
+    name: "Nora En Pure + Jonas Blue //",
+    description: "Once again we would like to welcome Nora En Pure back to San Francisco for a night of indie dance and deep house disco sounds to heat up the dance floor. Joinging her will be London based producer Jonas Blue, as well as more disco house DJ's to be announced - and we'll be dancing in a great downtown warehouse location. Your Friday night dance party option is here - grab your tickets and we'll see you there!",
+    street_line1: "470 Stevenson Street",
+    start_date: DateTime.httpdate('Friday, Nov 11 2016 21:05:06 GMT'),
+    end_date: DateTime.httpdate('Saturday, Nov 12 2016 02:05:06 GMT'),
+    city: "San Francisco",
+    state: "CA",
+    zip: "94103",
+    country: "US",
+    cancelled: false,
+    user_id: 3
+);
+
+Event.create(
+    name: "Dubfire At Kingdom",
+    description: "From global success in the commercial domain, to equal accomplishments within the underground scene, Dubfire (aka Ali Shirazinia) is an artist whose drive, talent and intuition have placed him in the top tier of electronic music artists in the world. He maintains this position within the industry with an unrelenting work ethic and a determination to innovate, evolve, and entertain. Whether it’s his involvement with Deep Dish, his solo performances at an extensive list of the world’s most influential clubs and festivals, his dubfire:live HYBRID show, running his label SCI+TEC, or simply his exploration of audio and visual technology, Dubfire is in a class of his own.",
+    street_line1: "859 O'Farrell Street",
+    start_date: DateTime.httpdate('Saturday, Feb 04 2017 21:05:06 GMT'),
+    end_date: DateTime.httpdate('Sunday, Feb 05 2017 02:05:06 GMT'),
+    city: "San Francisco",
+    state: "CA",
+    zip: "78701",
+    country: "US",
+    cancelled: false,
+    user_id: 3
+);
+
+Event.create(
+    name: "Anders Osborne / James Mc Murtry",
+    description: "ANDERS OSBORNE: The depth of one’s life is evident through their music. The more sorrow, laughter and adventure experienced, the more interesting curves and crevices are carved into an artist’s songs. The miles traveled leave rich lines in the verses that only time, misadventure, and hard-won wisdom can produce. Anders Osborne is a map of intensely felt, passionately engaged living, a fractured but healing topography of heartbreak and hope for fellow travelers to explore. Osborne’s music is redolent of the blues bathed in West Coast sunshine and brotherly compassion, a torchbearer for rock ‘n’ roll with blood in its veins and a heart in its hands. JAMES McMURTRY: Complicated Game, McMurtry’s first album in six years, has garnered universal acclaim. “At a stage where most veteran musicians fall into a groove or rut, McMurtry continues to surprise,” Texas Music magazine recently noted. “[Complicated Game] is a collection of narratives as sharply observed as any from McMurtry, but with a contemplative depth that comes with maturity.” Indeed, McMurtry’s latest collection spotlights a craftsman in absolutely peak form as he turns from the political toward the personal (“These Things I’ve Come to Know,” “You Got to Me”). “The lyrical theme is mostly about relationships,” the longtime Austin resident says. “It’s also a little about the big old world verses the poor little farmer or fisherman.” Either way, McMurtry spins his stories with a poet’s pen (“Long Island Sound”) and a painter’s precision (“She Loves Me”) throughout.",
+    street_line1: "859 O'Farrell Street",
+    start_date: DateTime.httpdate('Saturday, Nov 12 2016 22:00:00 GMT'),
+    end_date: DateTime.httpdate('Sunday, Nov 13 2017 02:00:00 GMT'),
+    city: "San Francisco",
+    state: "CA",
+    zip: "94101",
+    country: "US",
+    cancelled: true,
+    user_id: 3
+);
 
 
